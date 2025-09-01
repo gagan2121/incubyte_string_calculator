dc0dfaca0988f0681516c18e3cf6b2cbe5ff10a1
gagan <gagan21sahu@gmail.com>
2025-09-02 00:38:02 +0530

    add code for diff delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,25 @@ class StringCalculator
   def add(str)
     return 0 if str.empty?
 
-    str.split(/,|\n/).map(&:to_i).sum
+    separators = get_separators(str)
+    str = get_numbers(separators, str)
+    
+    str.reduce(:+)
+  end
+
+
+  def get_numbers(separators, string)
+    string.split(/#{Regexp.union(separators)}/).map(&:to_i)
+  end
+
+  def get_separators(string)
+    separators = [',', "\n"]
+    return separators unless includes_delimitier?(string)
+
+    separators << string.split("\n")[0][2..-1]
+  end
+
+  def includes_delimitier?(string)
+    string.start_with?('//')
   end
 end
